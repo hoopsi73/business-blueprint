60c6d3cc748596b347264aff1388d7a32a8b3167
Hubert Schagerl <124398173+hoopsi73@users.noreply.github.com>
2025-09-01 13:18:31 +0200

    Fix Storyblok client initialization error

--- src/lib/storyblok.ts
+++ src/lib/storyblok.ts
@@ -9,26 +9,36 @@ import Card from '../storyblok/Card'
 import Button from '../storyblok/Button'
 import { apiPlugin, storyblokInit } from '@storyblok/react/rsc'
 
-export const getStoryblokApi = storyblokInit({
-  accessToken: process.env.STORYBLOK_DELIVERY_API_TOKEN,
-  use: [apiPlugin],
-  components: {
-    page: Page,
-    teamMembers: TeamMembers,
-    testimonials: Testimonials,
-    testimonial: Testimonial,
-    cards: Cards,
-    card: Card,
-    hero: Hero,
-    tabs: Tabs,
-    button: Button,
-  },
-  apiOptions: {
-    /** Set the correct region for your space. Learn more: https:/www.storyblok.com/docs/packages/storyblok-js#example-region-parameter */
-    region: 'eu',
-    /** The following code is only required when creating a Storyblok space directly via the Blueprints feature. */
-    endpoint: process.env.STORYBLOK_API_BASE_URL
-      ? `${new URL(process.env.STORYBLOK_API_BASE_URL).origin}/v2`
-      : undefined,
-  },
-})
+export const getStoryblokApi = () => {
+  const accessToken = process.env.STORYBLOK_DELIVERY_API_TOKEN
+  
+  if (!accessToken) {
+    throw new Error(
+      'STORYBLOK_DELIVERY_API_TOKEN is not defined. Please set this environment variable with your Storyblok Delivery API token.'
+    )
+  }
+
+  return storyblokInit({
+    accessToken,
+    use: [apiPlugin],
+    components: {
+      page: Page,
+      teamMembers: TeamMembers,
+      testimonials: Testimonials,
+      testimonial: Testimonial,
+      cards: Cards,
+      card: Card,
+      hero: Hero,
+      tabs: Tabs,
+      button: Button,
+    },
+    apiOptions: {
+      /** Set the correct region for your space. Learn more: https:/www.storyblok.com/docs/packages/storyblok-js#example-region-parameter */
+      region: 'eu',
+      /** The following code is only required when creating a Storyblok space directly via the Blueprints feature. */
+      endpoint: process.env.STORYBLOK_API_BASE_URL
+        ? `${new URL(process.env.STORYBLOK_API_BASE_URL).origin}/v2`
+        : undefined,
+    },
+  })
+}
